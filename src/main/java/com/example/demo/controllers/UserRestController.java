130db4e11b2317f0620fb7fc78e120e702171bc8
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-12-09 11:15:06 +0100

    feat: Add RestController example

---
+++ src/main/java/com/example/demo/controllers/UserRestController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserRestController {
+
+    @GetMapping("/api/details")
+    public Map<String, Object> details() {
+
+        Map<String, Object> body = new HashMap<>();
+
+        body.put("title", "Details template");
+        body.put("name", "John");
+        body.put("lastname", "Doe");
+        return body;
+    }
+}
