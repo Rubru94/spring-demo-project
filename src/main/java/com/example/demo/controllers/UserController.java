0b325691d4d222ad425c3559f733ec6f052eebcb
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-12-09 09:04:15 +0100

    feat: Send data to the view

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,13 +1,21 @@
 package com.example.demo.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
     @GetMapping("details")
-    public String details() {
+    public String details(Model model, Map<String, Object> map) {
+
+        model.addAttribute("title", "Details template");
+        model.addAttribute("name", "John");
+        // model.addAttribute("lastname", "Doe");
+        map.put("lastname", "Does");
         return "details";
     }
 }
