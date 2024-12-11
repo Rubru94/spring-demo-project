ebe770ff9f6a84948d6e226e4a98917b2f216c0f
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-12-11 10:39:56 +0100

    feat: Directives & list with Thymeleaf

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.models.User;
@@ -15,9 +19,23 @@ public class UserController {
     public String details(Model model, Map<String, Object> map) {
 
         User user = new User("John", "Doe");
+        user.setEmail("example@example.com");
 
         model.addAttribute("title", "Details template");
         model.addAttribute("user", user);
         return "details";
+    }
+
+    @GetMapping("list")
+    public String list(ModelMap model) {
+        User user1 = new User("John", "Doe", "jd@gmail.com");
+        User user2 = new User("Arthur", "Morgan");
+        User user3 = new User("Elver", "Galarga", "elga@hotmail.com");
+
+        List<User> users = Arrays.asList(user1, user2, user3);
+        model.addAttribute("title", "List template");
+        model.addAttribute("users", users);
+
+        return "list";
     }
 }
--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -4,6 +4,7 @@ public class User {
 
     private String name;
     private String lastname;
+    private String email;
 
     public User() {
     }
@@ -11,6 +12,11 @@ public class User {
     public User(String name, String lastname) {
         this.name = name;
         this.lastname = lastname;
+    }
+
+    public User(String name, String lastname, String email) {
+        this(name, lastname);
+        this.email = email;
     }
 
     public String getName() {
@@ -27,5 +33,13 @@ public class User {
 
     public void setLastname(String lastname) {
         this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
