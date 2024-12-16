268c28ca9a0bdc5cfa6626837339daf2213825f9
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-12-16 16:12:24 +0100

    feat: Handle request object HttpServletRequest

--- src/main/java/com/example/demo/controllers/RequestParamsController.java
+++ src/main/java/com/example/demo/controllers/RequestParamsController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.models.dto.ParamDto;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/api/params")
 public class RequestParamsController {
 
+    /**
+     * @example request GET using annotation @RequestParam
+     */
     @GetMapping("example")
     public ParamDto getExample(@RequestParam(name = "msg", required = false, defaultValue = "empty") String message) {
 
@@ -21,12 +26,34 @@ public class RequestParamsController {
         return paramDto;
     }
 
+    /**
+     * @example request GET using annotation @RequestParam with several request
+     *          params
+     */
     @GetMapping("several-reqparams")
     public ParamDto getSeveralReqParams(@RequestParam String text, @RequestParam Integer code) {
 
         ParamDto paramDto = new ParamDto();
         paramDto.setMessage(text);
         paramDto.setCode(code);
+        return paramDto;
+    }
+
+    /**
+     * @example request GET using HttpServletRequest
+     */
+    @GetMapping("servlet-request")
+    public ParamDto getParamUsingServletRequest(HttpServletRequest request) {
+
+        Integer code = 0;
+        try {
+            code = Integer.parseInt(request.getParameter("code"));
+        } catch (Exception e) {
+        }
+
+        ParamDto paramDto = new ParamDto();
+        paramDto.setCode(code);
+        paramDto.setMessage(request.getParameter("text"));
         return paramDto;
     }
 
